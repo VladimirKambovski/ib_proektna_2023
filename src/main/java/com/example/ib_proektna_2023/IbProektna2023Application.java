c0f7525bb02200747fbfea6c523343706c893ac3
Kambovski <Vkambovski001@gmail.com>
2023-09-15 01:34:31 +0200

    New commit

--- src/main/java/com/example/ib_proektna_2023/IbProektna2023Application.java
+++ src/main/java/com/example/ib_proektna_2023/IbProektna2023Application.java
@@ -2,8 +2,9 @@ package com.example.ib_proektna_2023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class IbProektna2023Application {
 
 	public static void main(String[] args) {
