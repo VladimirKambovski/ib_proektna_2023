6ff7c044c49dcfdae7c7a6cc84e4c45c52e054b7
Kambovski <Vkambovski001@gmail.com>
2023-09-14 19:17:37 +0200

    initial commit

---
+++ src/main/java/com/example/ib_proektna_2023/IbProektna2023Application.java
@@ -0,0 +1,13 @@
+package com.example.ib_proektna_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbProektna2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbProektna2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ib_proektna_2023/IbProektna2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ib_proektna_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbProektna2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
